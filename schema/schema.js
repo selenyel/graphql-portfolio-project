c20667151a02faf25b87747eda652eb0d55737ba
Selen YEL <shiroshirochan@outlook.com>
2024-07-21 20:39:39 +0300

    Initial commit

---
+++ schema/schema.js
@@ -0,0 +1,71 @@
+const graphql = require('graphql');
+const axios = require('axios');
+
+const {GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLBoolean,GraphQLSchema,GraphQLList} = graphql;
+
+
+const CompanyType = new GraphQLObjectType({
+  name:'Company',
+  fields:() => (
+    {
+      id: {type : GraphQLString},
+      name:{type : GraphQLString},
+      desc:{type : GraphQLString},
+      users:{
+        type : new GraphQLList(UserType),
+        resolve(parentValue,args){
+          return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(resp => resp.data)
+        }
+      }
+    }
+  )
+})
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => (
+    {
+      id: {type: GraphQLString},
+      firstName: {type: GraphQLString},
+      lastName: {type: GraphQLString},
+      age: {type: GraphQLInt},
+      isDead: {type:GraphQLBoolean},
+      company:{
+        type:CompanyType,
+        resolve(parentValue,args){
+          return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(resp => resp.data)
+        }
+      }
+    }
+  )
+})
+
+const RootQuery = new GraphQLObjectType({
+  name:'RootQueryType',
+  fields: {
+    user:{
+      type:UserType,
+      args:{id:{type:GraphQLString}
+      // , isDead:{type:GraphQLBoolean}
+    },
+      resolve(parentValue,args){
+        // return _.find(users,{id:args.id,isDead:args.isDead})
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+        .then(resp => resp.data)
+      }
+    },
+    company:{
+      type:CompanyType,
+      args:{id:{type:GraphQLString}},
+      resolve(parentValue,args){
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+        .then(resp => resp.data)
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
